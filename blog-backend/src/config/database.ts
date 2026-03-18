8120bb3916bb0e0785dc6fd11287ee2593ebca08
xxnicor <nicoedu123@gmail.com>
2026-03-18 14:30:42 -0300

    refactor: ajustar formatação do código no arquivo database.ts

--- blog-backend/src/config/database.ts
+++ blog-backend/src/config/database.ts
@@ -1,11 +1,14 @@
+import dns from 'node:dns';
 import 'dotenv/config';
 import { Pool } from 'pg';
 
+dns.setDefaultResultOrder('ipv4first');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { 
-    rejectUnauthorized: false },
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.on('error', (error: Error) => {
